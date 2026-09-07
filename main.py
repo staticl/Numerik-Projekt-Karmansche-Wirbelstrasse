4e1fabe3095d5189fa03ad40fbd4839da3d0f43b
louis <100614627+staticl@users.noreply.github.com>
2026-09-07 17:41:38 +0200

    Fixed spelling mistakes in doc strings

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ from src.simulation import VortexSimulation
 def main():
     Re = 100  # Reynolds number, working point: 100 - 150
 
-    n = 156  # grid points in ξ-direction
-    m = 256  # grid points in θ-direction
+    n = 160  # grid points in ξ-direction
+    m = 160  # grid points in θ-direction
 
     simulation_time = 180  # total simulation time in seconds
 
     # Max dt for: m=n=160, dt=0.033; m=n=256, dt=0.02
-    simulation = VortexSimulation(Re, n, m, dt=0.02)
+    simulation = VortexSimulation(Re, n, m, dt=0.033)
     simulation.run_simulation(simulation_time, steps_per_frame=5, to_plot=True)
 
 
